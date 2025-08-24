d0ec40742ac7a0dca8bb125bb49125451ec0338d
fshangala <fshangala@gmail.com>
2025-08-24 07:40:19 +0200

    fix: update download links for Android and Windows components

--- src/android_download_component.jsx
+++ src/android_download_component.jsx
@@ -2,7 +2,7 @@ import androidLogo from './assets/android.png';
 export default function AndroidDownloadComponent() {
   return (                
     <a
-      href="#"
+      href="https://github.com/EXSAE-Technologies/exsaeller_landingpage/releases/download/v1.0.0/exsaeller-v1.0.0-app-release.apk"
       className="rounded-md bg-indigo-600 px-6 py-3 text-white hover:bg-indigo-700 flex items-center gap-2"
     >
       <img src={androidLogo} className='h-8 w-auto' alt='microsoft' />
--- src/windows_download_component.jsx
+++ src/windows_download_component.jsx
@@ -2,7 +2,7 @@ import microsoftLogo from './assets/microsoft.png';
 export default function WindowsDownloadComponent() {
   return (                
     <a
-      href="#"
+      href="https://github.com/EXSAE-Technologies/exsaeller_landingpage/releases/download/v1.0.0/exsaeller-v1.0.0-x64-setup.exe"
       className="rounded-md bg-indigo-600 px-6 py-3 text-white hover:bg-indigo-700 flex items-center gap-2"
     >
       <img src={microsoftLogo} className='h-8 w-auto' alt='microsoft' />

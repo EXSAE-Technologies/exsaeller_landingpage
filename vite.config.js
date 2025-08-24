5be4751719282004e7345a5c4b279d99e3ed4ae4
fshangala <fshangala@gmail.com>
2025-08-24 05:42:07 +0200

    updated base path

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'exsaeller_landingpage/',
+  base: '/exsaeller_landingpage/',
   plugins: [react(),tailwindcss()],
 })
